50cb1ea7aef9f171682281e7ba44d7ea9f41c015
Emily Welsch <emwelsch@gmail.com>
2018-02-16 18:00:42 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -136,17 +136,6 @@ def play(board)
   # Added conditions and puts
   if won?(board)
     puts "Congratulations, #{winner(board)}!"
-  else
-    puts "Cats Game!"
-  end
-end
-
-def play(board)
-  while !over?(board)
-    turn(board)
-  end
-  if won?(board)
-    puts "Congratulations #{winner(board)}!"
   elsif draw?(board)
     puts "Cat's Game!"
   end
